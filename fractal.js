82afb8b0e3091aef708fb5f4040a257a09b881c0
Thomas Bußmeyer <thomas.bussmeyer@publicispixelpark.de>
2017-09-05 16:18:21 +0200

    Add new test templates.

--- fractal.js
+++ fractal.js
@@ -18,17 +18,18 @@ fractal.set('project.title', 'Fractal Test');
 /*
  * Tell Fractal where to look for components.
  */
-fractal.components.set('path', path.join(__dirname, 'components'));
+fractal.components.set('path', path.join(__dirname, 'src/components'));
 fractal.components.set('ext', '.handlebars');
+fractal.components.set('default.preview', '@preview');
 
 /*
  * Tell Fractal where to look for documentation pages.
  */
-fractal.docs.set('path', path.join(__dirname, 'docs'));
+fractal.docs.set('path', path.join(__dirname, 'src/docs'));
 
 /*
  * Tell the Fractal web preview plugin where to look for static assets.
  */
 fractal.web.set('static.path', path.join(__dirname, 'public'));
 
-fractal.web.set('builder.dest', __dirname + '/build');
+fractal.web.set('builder.dest', path.join(__dirname, 'build'));
